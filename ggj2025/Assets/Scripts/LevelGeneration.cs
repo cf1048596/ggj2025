f3b9a142684a41f621155f78978bbc418f1616ca
cf1048596 <christianflorencio2004@gmail.com>
2025-01-25 13:01:32 +1300

    fixed level gen (pay attention to the video next time (retarded)

--- ggj2025/Assets/Scripts/LevelGeneration.cs
+++ ggj2025/Assets/Scripts/LevelGeneration.cs
@@ -7,10 +7,14 @@ public class LevelGeneration : MonoBehaviour
     public GameObject[] rooms;
     private int direction;
     public float moveAmount;
-
+    private bool stopGeneration;
 
     private float timeBtwRoom;
     public float startTimeBtwRoom = 0.25f;
+
+    public float minX;
+    public float maxX;
+    public float minY;
     public void Start()
     {
         int randStartingPos = Random.Range(0, startingPosition.Length);
@@ -22,7 +26,7 @@ public class LevelGeneration : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timeBtwRoom <= 0)
+        if (timeBtwRoom <= 0&&stopGeneration==false)
         {
             Move();
             timeBtwRoom = startTimeBtwRoom;
@@ -38,16 +42,36 @@ public class LevelGeneration : MonoBehaviour
     {
         if (direction == 1 || direction == 2)
         {
-            Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
-            transform.position = newPos;
+            if (transform.position.x < maxX)
+            {
+
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos;
+            } else
+            {
+                direction = 5;
+            }
+
         }
         else if (direction == 3 || direction == 4)
         {
-            Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
-            transform.position = newPos;
+            if (transform.position.x > minX)
+            {
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+            } else
+            {
+                direction = 5;
+            }
         } else if (direction == 5) { 
+            if (transform.position.y > minY) {
             Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
             transform.position = newPos;
+            }
+            else
+            {
+                stopGeneration = true;
+            }
         }
         Instantiate(rooms[0], transform.position, Quaternion.identity);
         direction = Random.Range(1, 6);
